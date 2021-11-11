58b12d8012269eac8f98cb80eb8e11215fac9a49
Prog24 <kenta@suzuki.dev>
2021-11-11 20:43:00 +0900

    fix PostList (#2)

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,6 +1,10 @@
-import { Container, List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import { Container, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import rssData from '../../../static/rss.json'
+
+import { ZennIcon } from '../Profile/assets/ZennIcon'
+import { NoteIcon } from '../Profile/assets/NoteIcon'
+import RssFeed from '@mui/icons-material/RssFeed'
 
 const PostListArea = styled(Container)(({ theme }) => ({
   [theme.breakpoints.down('md')]: {
@@ -16,6 +20,13 @@ const PostLists = styled(List)(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   padding: theme.spacing(2),
   borderRadius: theme.spacing(1),
+}))
+
+const PostMeta = styled(Typography)(({ theme }) => ({
+  display: 'inline-flex',
+  verticalAlign: 'text-bottom',
+  boxSizing: 'inherit',
+  alignItems: 'center',
 }))
 
 const Posts = () => {
@@ -34,7 +45,19 @@ const Posts = () => {
           return (
             <ListItem key={key} disablePadding divider>
               <ListItemButton rel='noopener noreferrer' disableGutters component='a' href={data.url} target='_blank'>
-                <ListItemText primaryTypographyProps={{color:'textPrimary'}} primary={data.title} secondary={`${hoge} - ${data.site}`} />
+                <ListItemText
+                  disableTypography
+                  primary={<Typography color='textPrimary'>{data.title}</Typography>}
+                  secondary={
+                    (data.site === 'Zenn' ? (
+                      <PostMeta color='textSecondary'>{hoge}　{<ZennIcon fontSize='small' sx={{color: '#1DA1F2'}} />} Posted on {data.site}</PostMeta>
+                    ) : (data.site === 'note' ? (
+                      <PostMeta color='textSecondary'>{hoge}　{<NoteIcon fontSize='small' sx={{color:'#41C9B4'}} />} Posted on {data.site}</PostMeta>
+                    ) : (
+                      <PostMeta color='textSecondary'>{hoge}　{<RssFeed fontSize='small' color='primary' />} Posted on {data.site}</PostMeta>
+                    )))
+                  }
+                />
               </ListItemButton>
             </ListItem>
           )
