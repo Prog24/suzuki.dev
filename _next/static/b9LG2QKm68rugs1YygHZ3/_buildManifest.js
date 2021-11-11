22147152d3e95baffa008555c798c14916247d90
Prog24 <Prog24@users.noreply.github.com>
2021-11-11 13:02:03 +0000

    deploy: ba7a1dbe21837ac1689e2f33a11947776cb3e177

--- _next/static/b9LG2QKm68rugs1YygHZ3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/387-ef22baf7526e9a49.js","static/chunks/pages/index-0ddb46ed2b35a4d8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
