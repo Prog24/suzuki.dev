f67a1da80f3b4c3187c0d36d9347f2863663ccd9
Prog24 <Prog24@users.noreply.github.com>
2021-11-29 08:04:35 +0000

    deploy: 66467562c6d830c19780a6770433894eb8300bdb

--- _next/static/zqaa1yOJPgavxMv5oIDXr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/390-6930ef54ba1e1690.js","static/chunks/pages/index-37d6a06bdb8f3ac7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
