017dbbbd0a60418abc5748c6fc36ce6c8c0ff2f2
Prog24 <Prog24@users.noreply.github.com>
2025-12-24 11:39:16 +0000

    deploy: 8832a7708ffcde5f29c190723938b717324ec6c9

---
+++ _next/static/chunks/1216.f93e3aa5297548a5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1216],{91216:(s,a,e)=>{e.r(a),e.d(a,{default:()=>n.Ay,getWasmInstance:()=>n.En,wasmBinary:()=>n.sh});var n=e(82950)}}]);
